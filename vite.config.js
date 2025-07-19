38bbbbc6c21f8ca5018c8dff94a943a9d5dae564
Your Name <you@example.com>
2025-07-20 02:22:00 +0900

    � 초기 커밋

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/MOM_BUS_Diary/', // 반드시 프로젝트명과 일치
+  base: '/mom-bus-diary/', // 반드시 프로젝트명과 일치
   plugins: [react()],
 });
